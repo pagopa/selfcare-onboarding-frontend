13d49644e1fcb0d9d3319d6be0153c784fca9978
MarioDolci9 <mario.dolci@everis.nttdata.com>
2021-10-26 09:03:20 +0200

    fixing bug OnBoardingStep 1 , clearing 'selected' in Autocomplete

--- src/components/AsyncAutocomplete.tsx
+++ src/components/AsyncAutocomplete.tsx
@@ -1,117 +1,118 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import debounce from 'lodash/debounce';
-import {AxiosResponse} from 'axios';
-import {Autocomplete, TextField} from '@mui/material';
-import {Box} from "@mui/system";
-import {Endpoint} from '../../types';
-import {fetchWithLogs} from '../lib/api-utils';
-import {getFetchOutcome} from '../lib/error-utils';
-import logo from "../assets/comune-milano-logo.svg";
+import { AxiosResponse } from 'axios';
+import { Autocomplete, TextField } from '@mui/material';
+import { Box } from '@mui/system';
+import { Endpoint } from '../../types';
+import { fetchWithLogs } from '../lib/api-utils';
+import { getFetchOutcome } from '../lib/error-utils';
+import logo from '../assets/comune-milano-logo.svg';
 
 type AutocompleteProps = {
-    selected: any;
-    setSelected: React.Dispatch<React.SetStateAction<any>>;
-    placeholder: string;
-    endpoint: Endpoint;
-    transformFn: any;
-    labelKey?: string;
+  selected: any;
+  setSelected: React.Dispatch<React.SetStateAction<any>>;
+  placeholder: string;
+  endpoint: Endpoint;
+  transformFn: any;
+  labelKey?: string;
 };
 
 export function AsyncAutocomplete({
-                                      selected,
-                                      setSelected,
-                                      placeholder,
-                                      endpoint,
-                                      transformFn,
-                                      labelKey,
-                                  }: AutocompleteProps) {
-    const [isLoading, setIsLoading] = useState(false);
-    const [input, setInput] = useState<string>();
-    const [options, setOptions] = useState<Array<any>>([]);
+  selected,
+  setSelected,
+  placeholder,
+  endpoint,
+  transformFn,
+  labelKey,
+}: AutocompleteProps) {
+  const [isLoading, setIsLoading] = useState(false);
+  const [input, setInput] = useState<string>();
+  const [options, setOptions] = useState<Array<any>>([]);
 
-    const handleSearch = async (query: string) => {
-        setIsLoading(true);
+  const handleSearch = async (query: string) => {
+    setIsLoading(true);
 
-        const searchResponse = await fetchWithLogs(endpoint, {
-            method: 'GET',
-            params: {limit: 100, page: 1, search: query},
-        });
+    const searchResponse = await fetchWithLogs(endpoint, {
+      method: 'GET',
+      params: { limit: 100, page: 1, search: query },
+    });
 
-        const outcome = getFetchOutcome(searchResponse);
+    const outcome = getFetchOutcome(searchResponse);
 
-        if (outcome === 'success') {
-            setOptions(transformFn((searchResponse as AxiosResponse).data));
+    if (outcome === 'success') {
+      setOptions(transformFn((searchResponse as AxiosResponse).data));
+    }
+    setIsLoading(false);
+  };
+
+  const noOptionsText =
+    input !== undefined && input.length >= 3 ? 'No risultati' : 'Digita almeno 3 caratteri';
+  const getOptionLabel: (option: any) => string =
+    labelKey !== undefined ? (o) => o[labelKey] : (o) => o.label ?? o;
+
+  return (
+    <Autocomplete
+      id="Parties"
+      freeSolo
+      value={selected}
+      noOptionsText={noOptionsText}
+      onChange={(_event, value) => setSelected(value)}
+      options={options}
+      loading={isLoading}
+      onInputChange={(_event, value, reason) => {
+        setInput(value);
+
+        if (reason === 'input') {
+          setSelected(null);
+          if (value.length >= 3) {
+            void debounce(handleSearch, 100)(value);
+          }
         }
-        setIsLoading(false);
-    };
-
-    const noOptionsText =
-        input !== undefined && input.length >= 3 ? 'No risultati' : 'Digita almeno 3 caratteri';
-    const getOptionLabel: (option: any) => string =
-        labelKey !== undefined ? (o) => o[labelKey] : (o) => o.label ?? o;
-
-    return (
-
-
-        <Autocomplete
-            id="Parties"
-            freeSolo
-            value={selected}
-            noOptionsText={noOptionsText}
-            onChange={(_event,value)=> setSelected(value)}
-            options={options}
-            loading={isLoading}
-            onInputChange={(_event, value, reason) => {
-                setInput(value);
-                if (reason === 'input' && value.length >= 3) {
-                    void debounce(handleSearch, 100)(value);
-                }
-            }}
-            filterOptions={(x) => x}
-            renderInput={(params) => (
-                    <TextField {...params}
-                               inputProps={{
-                                   style:
-                                       {
-                                           fontFamily: "Titillium Web",
-                                           fontStyle: "normal",
-                                           fontWeight: "normal",
-                                           fontSize: "16px",
-                                           lineHeight: "24px",
-                                           color: "#C1C9D2",
-                                           textAlign:"center"
-                                                                                  },
-                                   ...params.inputProps
-                               }}
-                               variant="standard"/>
-                )}
-            placeholder={placeholder}
-            getOptionLabel={getOptionLabel}
-            renderOption={(props, option) => (
-                <li {...props}>
-                    <Box sx={{width: 50}}>
-                        <img src={logo}></img>
-                    </Box>
-                    <Box
-                        sx={{
-                            fontFamily: "Titillium Web",
-                            fontStyle: "normal",
-                            fontWeight: "normal",
-                            fontSize: "16px",
-                            lineHeight: "24px",
-                            color: "#5A768A",
-                            textTransform: "capitalize",
-
-                        }}
-                    >
-                        {option.description?.toLowerCase()}
-                    </Box>
-                </li>
-
-            )}
-
+        if (reason === 'clear') {
+          setSelected(null);
+        }
+      }}
+      filterOptions={(x) => x}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          inputProps={{
+            style: {
+              fontFamily: 'Titillium Web',
+              fontStyle: 'normal',
+              fontWeight: 'normal',
+              fontSize: '16px',
+              lineHeight: '24px',
+              color: '#C1C9D2',
+              textAlign: 'center',
+            },
+            ...params.inputProps,
+          }}
+          variant="standard"
         />
-
-
-    );
+      )}
+      placeholder={placeholder}
+      getOptionLabel={getOptionLabel}
+      renderOption={(props, option) => (
+        <li {...props}>
+          <Box sx={{ width: 50 }}>
+            <img src={logo}></img>
+          </Box>
+          <Box
+            sx={{
+              fontFamily: 'Titillium Web',
+              fontStyle: 'normal',
+              fontWeight: 'normal',
+              fontSize: '16px',
+              lineHeight: '24px',
+              color: '#5A768A',
+              textTransform: 'capitalize',
+            }}
+          >
+            {option.description?.toLowerCase()}
+          </Box>
+        </li>
+      )}
+    />
+  );
 }
--- src/components/OnboardingStep1.tsx
+++ src/components/OnboardingStep1.tsx
@@ -1,72 +1,79 @@
-import {Grid, Stack, Typography} from '@mui/material';
-import {Link} from 'react-router-dom';
-import {Box} from "@mui/system";
-import {IPACatalogParty, StepperStepComponentProps} from '../../types';
-import {OnboardingStepActions} from './OnboardingStepActions';
-import {AsyncAutocomplete} from './AsyncAutocomplete';
-import {StyledIntro} from './StyledIntro';
-import {useHistoryState} from "./useHistoryState";
+import { Grid, Stack, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { Box } from '@mui/system';
+import { IPACatalogParty, StepperStepComponentProps } from '../../types';
+import { OnboardingStepActions } from './OnboardingStepActions';
+import { AsyncAutocomplete } from './AsyncAutocomplete';
+import { StyledIntro } from './StyledIntro';
+import { useHistoryState } from './useHistoryState';
 
-export function OnboardingStep1({forward}: StepperStepComponentProps) {
-    const [selected, setSelected,setSelectedHistory] = useHistoryState<IPACatalogParty>("selected_step1");
+export function OnboardingStep1({ forward }: StepperStepComponentProps) {
+  const [selected, setSelected, setSelectedHistory] = useHistoryState<IPACatalogParty | null>(
+    'selected_step1',
+    null
+  );
 
-    const onForwardAction = () => {
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        setSelectedHistory(selected);
-        const {digitalAddress, id} = /* TODO selected! */ {digitalAddress: 'asd', id: 'qwe'};
-        forward({institutionId: id}, digitalAddress);
-    };
+  const onForwardAction = () => {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    setSelectedHistory(selected);
+    const { digitalAddress, id } = selected!;
+    forward({ institutionId: id }, digitalAddress);
+  };
 
-    return (
-        <Stack spacing={10}>
-            <StyledIntro>
-                {{
-                    title: 'Seleziona il tuo Ente',
-                    description: (
-                        <>
-                            Seleziona dall’indice IPA l’Ente per cui vuoi richiedere l’adesione ai prodotti PagoPA
-                        </>
-                    ),
-                }}
-            </StyledIntro>
+  return (
+    <Stack spacing={10}>
+      <StyledIntro>
+        {{
+          title: 'Seleziona il tuo Ente',
+          description: (
+            <>
+              Seleziona dall’indice IPA l’Ente per cui vuoi richiedere l’adesione ai prodotti PagoPA
+            </>
+          ),
+        }}
+      </StyledIntro>
 
-            <Grid container justifyContent={'center'}>
-                <Grid item xs={12} md={6} lg={6}>
-                    <AsyncAutocomplete
-                        selected={selected}
-                        setSelected={setSelected}
-                        placeholder="Cerca ente nel catalogo IPA"
-                        endpoint={{endpoint: 'ONBOARDING_GET_SEARCH_PARTIES'}}
-                        transformFn={(data: { items: Array<IPACatalogParty> }) => data.items}
-                        labelKey="description"
-                    />
-                </Grid>
-            </Grid>
+      <Grid container justifyContent={'center'}>
+        <Grid item xs={12} md={6} lg={6}>
+          <AsyncAutocomplete
+            selected={selected}
+            setSelected={setSelected}
+            placeholder="Cerca ente nel catalogo IPA"
+            endpoint={{ endpoint: 'ONBOARDING_GET_SEARCH_PARTIES' }}
+            transformFn={(data: { items: Array<IPACatalogParty> }) => data.items}
+            labelKey="description"
+          />
+        </Grid>
+      </Grid>
 
+      <Box
+        sx={{
+          fontSize: '14px',
+          lineHeight: '24px',
+          textAlign: 'center',
+        }}
+      >
+        <Typography
+          sx={{
+            fontSize: '14px',
+            lineHeight: '24px',
+            textAlign: 'center',
+            color: '#17324D',
+          }}
+        >
+          Non trovi il tuo ente nell’indice IPA? <Link to="#">Clicca qui</Link> per maggiori
+          informazioni e istruzioni per essere inclusi nell’indice delle Pubbliche Amministrazioni
+        </Typography>
+      </Box>
 
-            <Box sx={{
-                fontSize: "14px",
-                lineHeight: "24px",
-                textAlign: "center"
-            }}>
-                <Typography sx={{
-                    fontSize: "14px",
-                    lineHeight: "24px",
-                    textAlign: "center",
-                    color: "#17324D"}}>
-                    Non trovi il tuo ente nell’indice IPA? <Link to="#">Clicca qui</Link> per maggiori
-                    informazioni e istruzioni per essere inclusi nell’indice delle Pubbliche Amministrazioni
-                </Typography>
-              </Box>
-
-            <OnboardingStepActions
-                // back={{action: back, label: 'Indietro', disabled: true}}
-                forward={{
-                    action: onForwardAction,
-                    label: 'Conferma',
-                    disabled: selected === undefined,
-                }}
-            />
-        </Stack>
-    );
+      <OnboardingStepActions
+        // back={{action: back, label: 'Indietro', disabled: true}}
+        forward={{
+          action: onForwardAction,
+          label: 'Conferma',
+          disabled: selected === undefined || selected === null,
+        }}
+      />
+    </Stack>
+  );
 }
