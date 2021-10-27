683dc4e38296531ddb7378fc6e39c7b28a5402b5
anttorre <antonio.torre@everis.com>
2021-10-27 18:12:20 +0200

    prepared to login integration

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -1,62 +1,11 @@
-import { useContext, useState } from 'react';
+import { useContext } from 'react';
 import isEmpty from 'lodash/isEmpty';
-import { AxiosResponse } from 'axios';
-import { Party } from '../../types';
-import { fetchAllWithLogs, fetchWithLogs } from '../lib/api-utils';
-import { URL_FE_LOGIN } from '../lib/constants';
-import { PartyContext, UserContext } from '../lib/context';
-import { isFetchError } from '../lib/error-utils';
+import { MOCK_USER, URL_FE_LOGIN } from '../lib/constants';
+import { UserContext } from '../lib/context';
 import { storageDelete, storageRead } from '../lib/storage-utils';
 
 export const useLogin = () => {
-  const [loadingText, setLoadingText] = useState<string | undefined>();
   const { setUser } = useContext(UserContext);
-  const { setAvailableParties, setParty } = useContext(PartyContext);
-
-  const setPartiesInContext = async (data: any) => {
-    // Store them in a variable
-    // eslint-disable-next-line functional/no-let
-    let parties: Array<Party> = data.institutions;
-    // Fetch all the partyIds (this can be optimized)
-    const partyIdsResponses = await fetchAllWithLogs(
-      parties.map(({ institutionId }) => ({
-        path: { endpoint: 'PARTY_GET_PARTY_ID', endpointParams: { institutionId } },
-        config: { method: 'GET' },
-      }))
-    );
-
-    // Associate each partyId to the correspondent party, along with its attributes
-    parties = parties.map((party) => {
-      const currentParty = (partyIdsResponses as Array<AxiosResponse>).find(
-        (r: AxiosResponse) => r.data.institutionId === party.institutionId
-      );
-
-      return {
-        ...party,
-        partyId: currentParty?.data.partyId,
-        attributes: currentParty?.data.attributes,
-      };
-    });
-
-    // Then set them
-    setAvailableParties(parties);
-  };
-
-  const fetchAndSetAvailableParties = async (taxCode: string) => {
-    setLoadingText('Stiamo associando la tua utenza ai tuoi enti');
-
-    // Get all available parties related to the user
-    const availablePartiesResponse = await fetchWithLogs(
-      { endpoint: 'ONBOARDING_GET_AVAILABLE_PARTIES', endpointParams: { taxCode } },
-      { method: 'GET' }
-    );
-
-    // If user already has institutions subscribed
-    if (!isFetchError(availablePartiesResponse)) {
-      // Set parties
-      await setPartiesInContext((availablePartiesResponse as AxiosResponse).data!);
-    }
-  };
 
   // This happens when the user does a hard refresh when logged in
   // Instead of losing the user, we attempt at logging it back in
@@ -64,12 +13,7 @@ export const useLogin = () => {
   // WARNING: this is not secure and will ultimately be rewritten
   // See PIN-403
   const attemptSilentLogin = async () => {
-    const sessionStorageUser = storageRead('user', 'object');
-    const sessionStorageParty = storageRead('currentParty', 'object');
-    const sessionStorageBearerToken = storageRead('bearer', 'string');
-
-    // TODO remove this entire if
-    if (isEmpty(sessionStorageUser)) {
+    if (MOCK_USER) {
       setUser({
         name: 'loggedName',
         surname: 'loggedSurname',
@@ -82,12 +26,12 @@ export const useLogin = () => {
       return;
     }
 
+    const sessionStorageUser = storageRead('user', 'object');
+
     // If there are no credentials, it is impossible to get the user, so
-    if (isEmpty(sessionStorageUser) || isEmpty(sessionStorageParty) || !sessionStorageBearerToken) {
+    if (isEmpty(sessionStorageUser)) {
       // Remove any partial data that might have remained, just for safety
       storageDelete('user');
-      storageDelete('currentParty');
-      storageDelete('bearer');
       // Go to the login view
       window.location.assign(URL_FE_LOGIN);
       // This return is necessary
@@ -96,13 +40,7 @@ export const useLogin = () => {
 
     // Otherwise, set the user to the one stored in the storage
     setUser(sessionStorageUser);
-
-    // Then fetch and set the parties available for this user
-    await fetchAndSetAvailableParties(sessionStorageUser.taxCode);
-
-    // In the end, set the user to the last known party
-    setParty(sessionStorageParty);
   };
 
-  return { attemptSilentLogin, loadingText };
+  return { attemptSilentLogin };
 };
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -9,8 +9,9 @@ import { IPAGuide } from '../views/IPAGuide' */
 const IS_DEVELOP = process.env.NODE_ENV === 'development';
 
 export const DISPLAY_LOGS = IS_DEVELOP;
+export const MOCK_USER = IS_DEVELOP;
 
-export const BASE_ROUTE = '';
+export const BASE_ROUTE = process.env.PUBLIC_URL ? process.env.PUBLIC_URL : '';
 
 export const URL_FE_LOGIN: string = process.env.REACT_APP_URL_FE_LOGIN;
 export const URL_FE_LOGOUT: string = process.env.REACT_APP_URL_FE_LOGOUT;
@@ -47,9 +48,6 @@ export const ROUTES: RoutesObject = {
 };
 
 export const API = {
-  BASE: {
-    URL: '',
-  },
   ONBOARDING_GET_AVAILABLE_PARTIES: {
     URL: URL_API_PARTY_PROCESS + '/onboarding/info/{{taxCode}}',
   },
