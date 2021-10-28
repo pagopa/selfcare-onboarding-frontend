fadc7671255549dffdb9539dd2df38062924cb30
MarioDolci <mario.dolci@everis.nttdata.com>
2021-10-28 08:40:16 +0200

    modificata responsiveness header e footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,85 +1,100 @@
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import Link from '@mui/material/Link';
-import { Box, Container } from '@mui/material';
-import LogoPagoPa from '../assets/pagopa-logo.svg';
+import {Box, Grid, Link, SvgIcon, Typography} from '@mui/material';
+import {ReactComponent as logo} from "../assets/logo_pago_pa_mini.svg";
 
 const Footer = () => (
-  <Box
-    component="footer"
-    sx={{
-      py: '50px',
-      px: 2,
-      mt: 'auto',
-      maxHeight: '189px',
-      bgcolor: '#01254C',
-    }}
-  >
-    <Container maxWidth="lg">
-      <Grid container direction="row" spacing={4}>
-        <Grid item xs={3} md={3} lg={3}>
-          <img src={LogoPagoPa} alt="fireSpot" />
-        </Grid>
-        <Grid item xs={9} md={9} lg={9}>
-          <Typography
-            component="div"
-            sx={{
-              fontWeight: 'normal',
-              fontSize: '15px',
-              lineHeight: '22,82px',
-              textAlign: 'center',
-              color: 'text.disabled',
-            }}
-          >
-            PagoPA S.p.A. - società per azioni con socio unico - capitale sociale di euro 1,000,000
-            interamente versato - sede legale in Roma, Piazza Colonna 370, CAP 00187 - n. di
-            iscrizione a Registro Imprese di Roma, CF e P.IVA 15376371009
-          </Typography>
-        </Grid>
-        <Typography
-          style={{
-            fontWeight: 'normal',
-            fontSize: '15px',
-            lineHeight: '15px',
-            textAlign: 'center',
-            color: 'text.secondary',
+    <Box
+        component="footer"
+        sx={{
+            py: '32px',
+            mt: 'auto',
+            bgcolor: '#01254C',
+            alignItems: "center"
+        }}
+    >
+        <Grid container
+              justifyContent={"center"}
+              alignItems={"center"}
 
-            padding: '40px 0px',
-          }}
-          component="div"
+
         >
-          <Link
-            href="https://www.pagopa.gov.it/it/privacy-policy/"
-            underline="none"
-            sx={{ margin: '10px', color: 'text.disabled' }}
-          >
-            {'Privacy Policy '}{' '}
-          </Link>
-          <Link
-            href="https://pagopa.portaleamministrazionetrasparente.it/"
-            underline="none"
-            sx={{ margin: '10px', color: 'text.disabled' }}
-          >
-            {'Società Trasparente '}{' '}
-          </Link>
-          <Link
-            href="https://www.pagopa.it/it/lavora-con-noi/"
-            underline="none"
-            sx={{ margin: '10px', color: 'text.disabled' }}
-          >
-            {'Lavora Con Noi '}{' '}
-          </Link>
-          <Link
-            href="https://www.pagopa.gov.it/it/privacy-policy/"
-            underline="none"
-            sx={{ margin: '10px', color: 'text.disabled' }}
-          >
-            {'Sicurezza '}{' '}
-          </Link>
-        </Typography>
-      </Grid>
-    </Container>
-  </Box>
+            <Box sx={{width: "90%", display: "flex"}}>
+                <SvgIcon component={logo} viewBox="0 0 80 24" sx={{width: '80px'}}/>
+                <Box sx={{textAlign: 'end', flexGrow: 1}}>
+                    <Typography
+                        component="div"
+                        sx={{
+                            fontWeight: 'normal',
+                            fontSize: '15px',
+                            lineHeight: '22,82px',
+                            textAlign: 'left',
+                            color: 'background.default',
+                            paddingLeft: '30px'
+
+                        }}
+                    >
+                        PagoPA S.p.A. - società per azioni con socio unico - capitale sociale di euro 1,000,000
+                        interamente versato - sede legale in Roma, Piazza Colonna 370, CAP 00187 - n. di
+                        iscrizione a Registro Imprese di Roma, CF e P.IVA 15376371009
+                    </Typography>
+                </Box>
+            </Box>
+
+
+        </Grid>
+        <Grid container
+              alignItems={"center"}
+              justifyContent={"center"}
+
+
+        >
+
+            <Box sx={{width: "90%", display: "flex"}}>
+
+                <Typography
+                    style={{
+                        fontWeight: 'normal',
+                        fontSize: '15px',
+                        lineHeight: '15px',
+                        textAlign: 'left',
+                        padding: '27px 0px',
+                    }}
+                    component="div"
+                >
+                    <Link
+                        href="https://www.pagopa.gov.it/it/privacy-policy/"
+                        underline="none"
+                        sx={{marginRight: '10px', color: 'text.disabled'}}
+                    >
+                        {'Privacy Policy '}{' '}
+                    </Link>
+                    <Link
+                        href="https://pagopa.portaleamministrazionetrasparente.it/"
+                        underline="none"
+                        sx={{margin: '10px', color: 'text.disabled'}}
+                    >
+                        {'Società Trasparente '}{' '}
+                    </Link>
+                    <Link
+                        href="https://www.pagopa.it/it/lavora-con-noi/"
+                        underline="none"
+                        sx={{margin: '10px', color: 'text.disabled'}}
+                    >
+                        {'Lavora Con Noi '}{' '}
+                    </Link>
+                    <Link
+                        href="https://www.pagopa.gov.it/it/privacy-policy/"
+                        underline="none"
+                        sx={{margin: '10px', color: 'text.disabled'}}
+                    >
+                        {'Sicurezza '}{' '}
+                    </Link>
+                </Typography>
+
+            </Box>
+        </Grid>
+
+
+    </Box>
 );
 
 export default Footer;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,19 @@
-import { AppBar, Container, Grid, Toolbar } from '@mui/material';
-import LogoPagoPa from '../assets/logo_pago_pa_mini.svg';
+import {AppBar, Button, SvgIcon, Toolbar} from '@mui/material';
+import {Box} from "@mui/system";
+import {ReactComponent as logo} from '../assets/logo_pago_pa_mini.svg';
 // import { URL_FE_LOGOUT } from '../lib/constants';
 
 const Header = () => (
-  <AppBar>
-    <Toolbar sx={{ bgcolor: 'main.primary' }}>
-      <Container maxWidth="lg">
-        <Grid container direction="row" spacing={4}>
-          <Grid item xs={3} md={3} lg={3}>
-            <img src={LogoPagoPa} alt="fireSpot" />
-          </Grid>
-          <Grid item xs={3} md={3} lg={3}>
-            <img src={LogoPagoPa} alt="fireSpot" />
-          </Grid>
-        </Grid>
-      </Container>
-    </Toolbar>
-  </AppBar>
+    <AppBar position="relative" sx={{alignItems:"center"}}>
+        <Toolbar sx={{ width:{xs:"100%",lg:"90%"} }}>
+            <SvgIcon component={logo} viewBox="0 0 80 24" sx={{ width: '80px' }} />
+            <Box sx={{ flexGrow: 1, textAlign: 'end' }}>
+                <Button variant="contained">
+                    Esci
+                </Button>
+            </Box>
+        </Toolbar>
+    </AppBar>
 );
 
 export default Header;
