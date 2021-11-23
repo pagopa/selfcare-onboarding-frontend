0e2f88f31459795e833705e6d099486899e5b0f5
Elena Signore <92723520+esignore@users.noreply.github.com>
2021-11-23 16:37:17 +0100

    SELC-258 textfield text color changed (#26)
    * SELC-258 textfield text color changed
    
    * SELC-258 removed comment

--- src/components/PlatformUserForm.tsx
+++ src/components/PlatformUserForm.tsx
@@ -1,9 +1,15 @@
 import { Grid, Paper, TextField } from '@mui/material';
 import React from 'react';
-// import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
-// import { Tooltip } from '@mui/material';
+import { styled } from '@mui/material/styles';
 import { UserOnCreate, UserPlatformRole, UserRole } from '../../types';
 import { UsersObject } from './OnboardingStep2';
+
+const CustomTextField = styled(TextField)({
+  '& .MuiFormHelperText-root':{
+    color: '#5C6F82',
+  },
+});
+
 type PlatformUserFormProps = {
   prefix: keyof UsersObject;
   role: UserRole;
@@ -77,13 +83,14 @@ export function PlatformUserForm({
   const errors: Array<string> = people[prefix] ? validateNoMandatory(people[prefix]) : [];
 
   return (
+
     <Paper elevation={0} sx={{ py:4, px:6 }} >
       <Grid container spacing={2}>
         {fields.map(({ id, label, type = 'text', width = 6, message, helperMessage}) => {
           const isError= errors.indexOf(id) > -1;
           return (
           <Grid item key={id} xs={width} mb={5}>
-            <TextField
+            <CustomTextField
               id={`${prefix}-${id}`}
               variant="standard"
               label={
