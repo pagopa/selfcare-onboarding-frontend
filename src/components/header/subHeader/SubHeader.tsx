67dec066bc520a3b7d8c69190853842d69f588d8
Elena Signore <92723520+esignore@users.noreply.github.com>
2021-11-23 17:01:01 +0100

    selc-262 boxShadow removed (#27)

--- src/components/header/subHeader/SubHeader.tsx
+++ src/components/header/subHeader/SubHeader.tsx
@@ -9,7 +9,7 @@ export default function SubHeader({children}: Props) {
   return (
     <AppBar
     position="relative"
-    sx={{ alignItems: 'center', height: '107px', backgroundColor: '#0066CC' }}
+    sx={{ alignItems: 'center', height: '107px', backgroundColor: '#0066CC', boxShadow:'none' }}
   >
     <Toolbar sx={{ width: { xs: '100%', lg: '90%' }, minHeight: '107px !important' }}>
       <Grid container>
