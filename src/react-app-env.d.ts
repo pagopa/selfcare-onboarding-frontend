d3dacef08e07b787282d7f07a87126463a61d664
anttorre <antonio.torre@everis.com>
2021-11-25 15:13:19 +0100

    devops-updated

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -3,13 +3,16 @@
 declare namespace NodeJS {
   interface ProcessEnv {
     NODE_ENV: 'development' | 'uat' | 'production';
+
     REACT_APP_URL_FE_LOGIN: string;
     REACT_APP_URL_FE_LOGOUT: string;
     REACT_APP_URL_FE_DASHBOARD: string;
+    REACT_APP_URL_FE_LANDING: string;
+
     REACT_APP_URL_API_PARTY_PROCESS: string;
     REACT_APP_URL_API_PARTY_REGISTRY_PROXY: string;
     REACT_APP_URL_API_PARTY_MANAGEMENT: string;
-    REACT_APP_URL_FE_LANDING: string;
+
     REACT_APP_PAGOPA_HELP_EMAIL: string;
   }
 }
