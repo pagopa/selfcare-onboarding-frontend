b74c123b58df468c92f8e74f32d628789eb1090f
USERSAD\esignore <elena.signore@nttdata.com>
2022-03-18 18:38:40 +0100

    SELC-685

--- src/components/AlertDialog.tsx
+++ src/components/AlertDialog.tsx
@@ -3,6 +3,7 @@ import Dialog from '@mui/material/Dialog';
 import DialogContent from '@mui/material/DialogContent';
 import { DialogActions, Grid, Typography } from '@mui/material';
 import ClearOutlinedIcon from '@mui/icons-material/ClearOutlined';
+import { useTranslation } from 'react-i18next';
 
 type DialogOption = {
   title: string | null;
@@ -25,6 +26,7 @@ export function AlertDialog({
   handleConfirm,
   handleClose,
 }: DialogOption) {
+  const { t } = useTranslation();
   return (
     <Dialog
       open={open}
@@ -48,7 +50,7 @@ export function AlertDialog({
           variant={'h6'}
           align="left"
         >
-          {title ? title : 'Titolo'}
+          {title ? title : t('alertDialog.title')}
         </Typography>
 
         <ClearOutlinedIcon onClick={handleClose} sx={{ color: 'info.main' }} />
@@ -56,7 +58,7 @@ export function AlertDialog({
 
       <DialogContent>
         <Typography variant={'body1'} align="left">
-          {description ? description : 'Descrizione'}
+          {description ? description : t('alertDialog.description')}
         </Typography>
       </DialogContent>
       <DialogActions>
@@ -67,11 +69,11 @@ export function AlertDialog({
               variant="contained"
               onClick={handleConfirm ? handleConfirm : handleClose}
             >
-              {confirmLabel ? confirmLabel : 'Torna alla pagina di caricamento'}
+              {confirmLabel ? confirmLabel : t('alertDialog.confirmLabel')}
             </Button>
           )}
           <Button sx={{ mt: 1 }} color="primary" variant="outlined" onClick={handleClose}>
-            {cancelLabel ? cancelLabel : 'Esci'}
+            {cancelLabel ? cancelLabel : t('alertDialog.cancelLabel')}
           </Button>
         </Grid>
       </DialogActions>
---
+++ src/locale/it.ts
@@ -0,0 +1,8 @@
+export default {
+  alertDialog: {
+    title: 'Titolo',
+    description: 'Descrizione',
+    confirmLabel: 'Torna alla pagina di caricamento',
+    cancelLabel: 'Esci',
+  },
+};
