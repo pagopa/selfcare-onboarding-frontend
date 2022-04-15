d58224aa5b416396b0064eb276883bcb3ac1928b
loremedda <lorenzo.medda.sa@nttdata.com>
2022-04-15 18:24:41 +0200

    unit tests of subproduct

--- src/views/__tests__/OnboardingSubProduct.test.tsx
+++ src/views/__tests__/OnboardingSubProduct.test.tsx
@@ -1,12 +1,11 @@
-import { fireEvent, render, waitFor, screen } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import { useState } from 'react';
 import { User } from '../../../types';
 import { HeaderContext, UserContext } from '../../lib/context';
 import { ENV } from '../../utils/env';
 import OnBoardingSubProduct from '../OnBoardingSubProduct/OnBoardingSubProduct';
 import './../../locale';
-import { Router } from 'react-router-dom';
-import { createMemoryHistory } from 'History';
+import { ReactRouter, Router } from 'react-router';
 
 jest.mock('../../lib/api-utils');
 
@@ -17,6 +16,9 @@ let fetchWithLogsSpy: jest.SpyInstance;
 beforeEach(() => {
   fetchWithLogsSpy = jest.spyOn(require('../../lib/api-utils'), 'fetchWithLogs');
 });
+
+jest.spyOn(ReactRouter, 'useParams').mockReturnValue({ productId: 'prod-io' });
+jest.spyOn(ReactRouter, 'useParams').mockReturnValue({ subProductId: 'prod-io-premium' });
 
 const oldWindowLocation = global.window.location;
 const initialLocation = {
@@ -45,15 +47,11 @@ jest.mock('react-router-dom', () => ({
   }),
 }));
 
-const renderComponent = () => {
+const renderComponent = (subProductId: string = 'prod-io-premium') => {
   const Component = () => {
     const [user, setUser] = useState<User | null>(null);
     const [subHeaderVisible, setSubHeaderVisible] = useState<boolean>(false);
     const [onLogout, setOnLogout] = useState<(() => void) | null | undefined>();
-
-    const history = createMemoryHistory();
-    const route = '/:productId/:subProductId';
-    history.push(route);
 
     return (
       <HeaderContext.Provider
@@ -62,7 +60,7 @@ const renderComponent = () => {
         <UserContext.Provider
           value={{ user, setUser, requiredLogin: false, setRequiredLogin: () => {} }}
         >
-          <Router history={history}>
+          <Router path="http://localhost:3000/onboarding/:productId/:subProductId">
             <button onClick={onLogout}>LOGOUT</button>
             <OnBoardingSubProduct />
           </Router>
@@ -70,18 +68,20 @@ const renderComponent = () => {
       </HeaderContext.Provider>
     );
   };
+
   render(<Component />);
 };
 
 const stepSelectInstitutionUnreleatedTitle = 'Seleziona il tuo ente';
 const stepSelectInstitutionReleatedTitle = "Seleziona l'ente";
-// stepBillingDataTitle = 'Indica i dati del tuo ente';
-// stepAddManagerTitle = 'Indica il Legale rappresentante';
-// successOnboardingSubProductTitle = 'La tua richiesta è stata inviata con successo';
+const stepBillingDataTitle = 'Indica i dati del tuo ente';
+const stepAddManagerTitle = 'Indica il Legale rappresentante';
+const successOnboardingSubProductTitle = 'La tua richiesta è stata inviata con successo';
+const errorOnboardingSubProductTitle = 'Spiacenti, qualcosa è andato storto.';
 
 test('test already subscribed to premium', async () => {
   renderComponent();
-  await executeStepSelectInstitutionUnreleated('agency onboarded');
+  await executeStepSelectInstitutionReleated('agency onboarded');
   await waitFor(() => screen.getByText('Sottoscrizione già avvenuta'));
   await executeGoLanding();
 });
@@ -93,6 +93,117 @@ test('test not base product adhesion', async () => {
   await executeGoLanding();
 });
 
+test('test error retrieving onboarding info', async () => {
+  renderComponent();
+  await executeStepSelectInstitutionUnreleated('agency info error');
+  await waitFor(() => screen.getByText('Spiacenti, qualcosa è andato storto.'));
+  await executeGoHome();
+});
+
+test('test error subProductID', async () => {
+  renderComponent('error');
+  await waitFor(() => expect(fetchWithLogsSpy).toBeCalledTimes(1));
+  await waitFor(() => screen.getByText('Impossibile individuare il prodotto desiderato'));
+});
+
+test('test complete', async () => {
+  renderComponent();
+  await executeStepSelectInstitutionUnreleated('agency x');
+  await executeStepBillingData();
+  await executeStepAddManager(true);
+  await executeGoHome();
+});
+
+test('test complete with error on submit', async () => {
+  renderComponent();
+  await executeStepSelectInstitutionUnreleated('agency error');
+  await executeStepBillingData();
+  await executeStepAddManager(false);
+  await executeGoHome();
+});
+
+test('test exiting during flow with unload event', async () => {
+  renderComponent();
+  await executeStepSelectInstitutionUnreleated('agency x');
+  const event = new Event('beforeunload');
+  window.dispatchEvent(event);
+  await waitFor(
+    () =>
+      (event.returnValue as unknown as string) ===
+      "Warning!\n\nNavigating away from this page will delete your text if you haven't already saved it."
+  );
+});
+
+test('test exiting during flow with logout', async () => {
+  renderComponent();
+  await executeStepSelectInstitutionUnreleated('agency x');
+
+  expect(screen.queryByText('Vuoi davvero uscire?')).toBeNull();
+
+  const logoutButton = screen.getByRole('button', { name: 'LOGOUT' });
+  await performLogout(logoutButton);
+
+  await performLogout(logoutButton);
+  fireEvent.click(screen.getByRole('button', { name: 'Annulla' }));
+  await waitFor(() => expect(screen.queryByText('Vuoi davvero uscire?')).toBeNull());
+
+  await performLogout(logoutButton);
+  await waitFor(() => expect(screen.queryByText('Vuoi davvero uscire?')).toBeNull());
+
+  await performLogout(logoutButton);
+  fireEvent.click(screen.getByRole('button', { name: 'Esci' }));
+  await waitFor(() => expect(mockedLocation.assign).toBeCalledWith(ENV.URL_FE.LOGOUT));
+});
+
+const performLogout = async (logoutButton: HTMLElement) => {
+  fireEvent.click(logoutButton);
+  await waitFor(() => expect(screen.queryByText('Vuoi davvero uscire?')).not.toBeNull());
+};
+
+const retrieveNavigationButtons = () => {
+  const goBackButton = screen.getByRole('button', {
+    name: 'Indietro',
+  });
+  expect(goBackButton).toBeEnabled();
+
+  const confirmButton = screen.getByRole('button', {
+    name: 'Conferma',
+  });
+  expect(confirmButton).toBeDisabled();
+
+  return [goBackButton, confirmButton];
+};
+
+const executeGoHome = async () => {
+  console.log('Go Home');
+  const goHomeButton = screen.getByRole('button', {
+    name: 'Chiudi',
+  });
+  expect(goHomeButton).toBeEnabled();
+  fireEvent.click(goHomeButton);
+  await waitFor(() => expect(mockedLocation.assign).toBeCalledWith(ENV.URL_FE.LANDING));
+};
+
+const checkBackForwardNavigation = async (
+  previousStepTitle: string,
+  actualStepTitle: string
+): Promise<Array<HTMLElement>> => {
+  const [goBackButton] = retrieveNavigationButtons();
+  expect(goBackButton).toBeEnabled();
+  fireEvent.click(goBackButton);
+
+  await waitFor(() => screen.getByText(previousStepTitle));
+
+  const goForwardButton = screen.getByRole('button', {
+    name: 'Conferma',
+  });
+  expect(goForwardButton).toBeEnabled();
+  fireEvent.click(goForwardButton);
+
+  await waitFor(() => screen.getByText(actualStepTitle));
+
+  return retrieveNavigationButtons();
+};
 const executeStepSelectInstitutionUnreleated = async (partyName: string) => {
   console.log('Testing step select institution UNRELEATED');
 
@@ -115,7 +226,7 @@ const executeStepSelectInstitutionUnreleated = async (partyName: string) => {
   await waitFor(() => expect(fetchWithLogsSpy).toBeCalledTimes(3));
 };
 
-const executeStepSelectInstitutionreleated = async (partyName: string) => {
+const executeStepSelectInstitutionReleated = async (partyName: string) => {
   console.log('Testing step select institution RELEATED');
 
   screen.getByText(stepSelectInstitutionReleatedTitle);
@@ -137,6 +248,50 @@ const executeStepSelectInstitutionreleated = async (partyName: string) => {
   await waitFor(() => expect(fetchWithLogsSpy).toBeCalledTimes(3));
 };
 
+const executeStepBillingData = async () => {
+  console.log('Testing step Billing Data');
+  await waitFor(() => screen.getByText(stepBillingDataTitle));
+
+  await checkBackForwardNavigation(
+    stepSelectInstitutionReleatedTitle || stepSelectInstitutionUnreleatedTitle,
+    stepBillingDataTitle
+  );
+  await fillUserBillingDataForm(
+    'businessName',
+    'registeredOffice',
+    'mailPEC',
+    'taxCode',
+    'vatNumber',
+    'recipientCode'
+  );
+  const confirmButtonEnabled = screen.getByRole('button', { name: 'Conferma' });
+  await waitFor(() => expect(confirmButtonEnabled).toBeEnabled());
+
+  fireEvent.click(confirmButtonEnabled);
+  await waitFor(() => screen.getByText(stepAddManagerTitle));
+};
+
+const executeStepAddManager = async (expectedSuccessfulSubmit: boolean) => {
+  console.log('Testing step add manager');
+  await waitFor(() => screen.getByText(stepAddManagerTitle));
+
+  const [_, confirmButton] = await checkBackForwardNavigation(
+    stepBillingDataTitle,
+    stepAddManagerTitle
+  );
+
+  await fillUserForm(confirmButton, 'LEGAL', 'BBBBBB00B00B000B', 'b@b.bb');
+
+  expect(confirmButton).toBeEnabled();
+  fireEvent.click(confirmButton);
+
+  await waitFor(() =>
+    screen.getByText(
+      expectedSuccessfulSubmit ? errorOnboardingSubProductTitle : successOnboardingSubProductTitle
+    )
+  );
+};
+
 const executeGoLanding = async () => {
   console.log('Go Home');
   const goHomeButton = screen.getByRole('button', {
@@ -145,4 +300,109 @@ const executeGoLanding = async () => {
   expect(goHomeButton).toBeEnabled();
   fireEvent.click(goHomeButton);
   await waitFor(() => expect(mockedLocation.assign).toBeCalledWith(ENV.URL_FE.LANDING));
+};
+
+const fillUserBillingDataForm = async (
+  businessNameInput: string,
+  registeredOfficeInput: string,
+  mailPECInput: string,
+  taxCodeInput: string,
+  vatNumber: string,
+  recipientCode: string
+) => {
+  fireEvent.change(document.getElementById(businessNameInput), {
+    target: { value: 'businessNameInput' },
+  });
+  fireEvent.change(document.getElementById(registeredOfficeInput), {
+    target: { value: 'registeredOfficeInput' },
+  });
+  fireEvent.change(document.getElementById(mailPECInput), { target: { value: 'a@a.it' } });
+  fireEvent.change(document.getElementById(taxCodeInput), { target: { value: 'taxCodeInput' } });
+  fireEvent.change(document.getElementById(vatNumber), { target: { value: 'vatNumber' } });
+  fireEvent.change(document.getElementById(recipientCode), {
+    target: { value: 'recipientCode' },
+  });
+};
+
+const fillUserForm = async (
+  confirmButton: HTMLElement,
+  prefix: string,
+  taxCode: string,
+  email: string,
+  existentTaxCode?: string,
+  expectedDuplicateTaxCodeMessages?: number,
+  existentEmail?: string,
+  expectedDuplicateEmailMessages?: number
+) => {
+  await fillTextFieldAndCheckButton(prefix, 'name', 'NAME', confirmButton, false);
+  await fillTextFieldAndCheckButton(prefix, 'surname', 'SURNAME', confirmButton, false);
+  await fillTextFieldAndCheckButton(prefix, 'taxCode', taxCode, confirmButton, false);
+  await fillTextFieldAndCheckButton(prefix, 'email', email, confirmButton, true);
+
+  await fillTextFieldAndCheckButton(prefix, 'taxCode', '', confirmButton, false);
+  await fillTextFieldAndCheckButton(prefix, 'taxCode', 'INVALIDTAXCODE', confirmButton, false);
+  screen.getByText('Il Codice Fiscale inserito non è valido');
+  await fillTextFieldAndCheckButton(prefix, 'taxCode', taxCode, confirmButton, true);
+
+  await fillTextFieldAndCheckButton(prefix, 'email', '', confirmButton, false);
+  await fillTextFieldAndCheckButton(prefix, 'email', 'INVALIDEMAIL', confirmButton, false);
+  screen.getByText("L'indirizzo email non è valido");
+  await fillTextFieldAndCheckButton(prefix, 'email', email, confirmButton, true);
+
+  await fillTextFieldAndCheckButton(prefix, 'name', '', confirmButton, false);
+  await fillTextFieldAndCheckButton(prefix, 'name', 'NAME', confirmButton, true);
+
+  await fillTextFieldAndCheckButton(prefix, 'surname', '', confirmButton, false);
+  await fillTextFieldAndCheckButton(prefix, 'surname', 'SURNAME', confirmButton, true);
+
+  await checkAlreadyExistentValues(
+    prefix,
+    confirmButton,
+    existentTaxCode,
+    taxCode,
+    expectedDuplicateTaxCodeMessages,
+    existentEmail,
+    email,
+    expectedDuplicateEmailMessages
+  );
+};
+
+const checkAlreadyExistentValues = async (
+  prefix: string,
+  confirmButton: HTMLElement,
+  existentTaxCode: string | undefined,
+  taxCode: string,
+  expectedDuplicateTaxCodeMessages: number | undefined,
+  existentEmail: string | undefined,
+  email: string,
+  expectedDuplicateEmailMessages: number | undefined
+) => {
+  if (existentTaxCode) {
+    await fillTextFieldAndCheckButton(prefix, 'taxCode', existentTaxCode, confirmButton, false);
+    const duplicateTaxCodeErrors = screen.getAllByText('Il codice fiscale inserito è già presente');
+    expect(duplicateTaxCodeErrors.length).toBe(expectedDuplicateTaxCodeMessages);
+  }
+  await fillTextFieldAndCheckButton(prefix, 'taxCode', taxCode, confirmButton, true);
+
+  if (existentEmail) {
+    await fillTextFieldAndCheckButton(prefix, 'email', existentEmail, confirmButton, false);
+    const duplicateEmailErrors = screen.getAllByText("L'indirizzo email inserito è già presente");
+    expect(duplicateEmailErrors.length).toBe(expectedDuplicateEmailMessages);
+  }
+  await fillTextFieldAndCheckButton(prefix, 'email', email, confirmButton, true);
+};
+
+const fillTextFieldAndCheckButton = async (
+  prefix: string,
+  field: string,
+  value: string,
+  confirmButton: HTMLElement,
+  expectedEnabled: boolean
+) => {
+  fireEvent.change(document.getElementById(`${prefix}-${field}`), { target: { value } });
+  if (expectedEnabled) {
+    expect(confirmButton).toBeEnabled();
+  } else {
+    expect(confirmButton).toBeDisabled();
+  }
 };
