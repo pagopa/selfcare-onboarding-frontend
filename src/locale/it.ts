2019b59234987bbca451d0fc2184721f9106a8c1
loremedda <lorenzo.medda.sa@nttdata.com>
2022-04-22 10:07:32 +0200

    commit for fix conflicts

--- src/locale/it.ts
+++ src/locale/it.ts
@@ -256,7 +256,7 @@ export default {
     vatNumber: 'Partita IVA',
     recipientCode: 'Codice destinatario',
     recipientCodeDescription: 'È il codice necessario per ricevere le fatture elettroniche',
-    gspDescription: 'Sono gestore di almeno uno dei pubblici servizi Gas, Luce e/o TELCO',
+    gspDescription: 'Sono gestore di almeno uno dei pubblici servizi: Gas, Energia, Telco.',
     backLabel: 'Indietro',
     confirmLabel: 'Conferma',
   },
