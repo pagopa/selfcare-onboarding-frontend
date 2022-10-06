4fa467ec7e63bc2e3ff3205aaa4e3df24b02f8e3
USERSAD\esignore <elena.signore@nttdata.com>
2022-10-06 19:06:27 +0200

    SELC-1607

--- src/views/onboarding/Onboarding.tsx
+++ src/views/onboarding/Onboarding.tsx
@@ -436,7 +436,6 @@ function OnboardingComponent({ productId }: { productId: string }) {
               setActiveStep(0);
             } else if (fromDashboard && productAvoidStep) {
               setOnExitAction(() => () => history.goBack());
-
               setOpenExitModal(true);
             } else {
               setActiveStep(1);
