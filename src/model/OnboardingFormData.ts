5fe5cb879411edfc20fbce1b3fc9c4b9334c4e4a
Elena Signore <92723520+esignore@users.noreply.github.com>
2022-12-05 14:47:11 +0100

    Refactor billing data form

---
+++ src/model/OnboardingFormData.ts
@@ -0,0 +1,36 @@
+
+export type OnboardingFormData = {
+    // Ragione sociale
+    businessName: string;
+    // Sede legale
+    registeredOffice: string;
+    // Codice di avviamento postale
+    zipCode: string;
+    // Indirizzo PEC
+    digitalAddress: string;
+    // Codice fiscale
+    taxCode: string;
+    // Partita iva
+    vatNumber: string;
+    // Codice destinatario
+    recipientCode: string;
+    // servizi pubblici
+    publicServices?: boolean;
+    // n. iscrizione al Registro delle Imprese
+    commercialRegisterNumber?: string;
+    // iscrizione all'Albo
+    registrationInRegister?: string;
+    // numero iscrizione albo
+    registerNumber?: string;
+    // codice ABI
+    abiCode?: string;
+    // La partita IVA è di gruppo
+    vatNumberGroup?: boolean;
+    // Indirizzo DPO
+    dpoAddress?: string;
+    // Indirizzo PEC DPO
+    dpoPecAddress?: string;
+    // Indirizzo Email DPO
+    dopEmailAddress?: string;
+  };
+  
--- src/views/OnBoardingSubProduct/components/SubProductStepSubmit.tsx
+++ src/views/OnBoardingSubProduct/components/SubProductStepSubmit.tsx
@@ -3,7 +3,6 @@ import { trackEvent } from '@pagopa/selfcare-common-frontend/services/analyticsS
 import { Trans } from 'react-i18next';
 import { AxiosError } from 'axios';
 import {
-  BillingData,
   InstitutionType,
   Problem,
   Product,
@@ -15,11 +14,11 @@ import { fetchWithLogs } from '../../../lib/api-utils';
 import { getFetchOutcome } from '../../../lib/error-utils';
 import { MessageNoAction } from '../../../components/MessageNoAction';
 import { unregisterUnloadEvent } from '../../../utils/unloadEvent-utils';
-import {
-  billingData2billingDataRequest,
-  pspData2pspDataRequest,
-} from '../../onboarding/Onboarding';
+import {} from '../../onboarding/Onboarding';
 import ErrorPage from '../../../components/errorPage/ErrorPage';
+import { billingData2billingDataRequest } from '../../../model/BillingData';
+import { pspData2pspDataRequest } from '../../../model/PspData';
+import { OnboardingFormData } from '../../../model/OnboardingFormData';
 
 type Props = StepperStepComponentProps & {
   requestId: string;
@@ -27,7 +26,7 @@ type Props = StepperStepComponentProps & {
   subProduct: Product;
   externalInstitutionId: string;
   users: Array<UserOnCreate>;
-  billingData: BillingData;
+  billingData: OnboardingFormData;
   institutionType: InstitutionType;
   pricingPlan?: string;
   origin: string;
@@ -110,10 +109,10 @@ function SubProductStepSubmit({
             taxCode: u.taxCode.toUpperCase(),
             email: u.email.toLowerCase(),
           })),
-          billingData: billingData2billingDataRequest(billingData as BillingData),
+          billingData: billingData2billingDataRequest(billingData as OnboardingFormData),
           pspData:
             institutionType === 'PSP'
-              ? pspData2pspDataRequest(billingData as BillingData)
+              ? pspData2pspDataRequest(billingData as OnboardingFormData)
               : undefined,
           institutionType,
           pricingPlan,
