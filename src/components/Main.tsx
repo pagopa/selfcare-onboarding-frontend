3f4c86701dd4a5d28af6ede4dc04e24eae2ae9d6
loremedda <lorenzo.medda@nttdata.com>
2022-12-13 12:08:33 +0100

    removed minHeight

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -7,7 +7,7 @@ export function Main() {
   const location = useLocation();
   const productId = location.pathname.replace(/^\/onboarding\/([^/]+)\/?.*/, '$1');
   return (
-    <Box component="main" my="auto" marginY={'87px'} minHeight={'52vh'}>
+    <Box component="main" my="auto" marginY={'87px'}>
       <Switch>
         {Object.values(ROUTES).map(({ PATH, EXACT, COMPONENT: Component }, i) => (
           <Route path={PATH} exact={EXACT} key={i}>
