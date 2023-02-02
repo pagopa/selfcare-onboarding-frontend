afebde428c0b3d2876d64a797db4685b64fdd693
esignore <elena.signore@nttdata.com>
2023-02-02 15:21:52 +0100

    SELC-1964 - removed control from mìpremium

--- src/components/steps/StepOnboardingFormData.tsx
+++ src/components/steps/StepOnboardingFormData.tsx
@@ -321,11 +321,12 @@ export default function StepOnboardingFormData({
           values.shareCapital &&
           !currencyField.test(values.shareCapital) &&
           t('onboardingFormData.billingDataSection.invalidShareCapitalField'),
-        supportEmail: !values.supportEmail
-          ? requiredError
-          : !mailPECRegexp.test(values.supportEmail)
-          ? t('onboardingFormData.billingDataSection.invalidMailSupport')
-          : undefined,
+        supportEmail:
+          !values.supportEmail && !premiumFlow
+            ? requiredError
+            : !mailPECRegexp.test(values.supportEmail as string) && !premiumFlow
+            ? t('onboardingFormData.billingDataSection.invalidMailSupport')
+            : undefined,
       }).filter(([_key, value]) => value)
     );
 
