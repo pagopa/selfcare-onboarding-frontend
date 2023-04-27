6b8dea7a01137c7e10d0ce6524722d74c234e966
esignore <elena.signore@nttdata.com>
2023-04-27 16:06:19 +0200

    SELC-2275 - changed geotaxonomy redirect url

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -28,8 +28,7 @@ export const ENV = {
     PARTY_PROCESS: env.get('REACT_APP_URL_API_PARTY_PROCESS').required().asString(),
     ONBOARDING: env.get('REACT_APP_URL_API_ONBOARDING').required().asString(),
     PARTY_REGISTRY_PROXY: env.get('REACT_APP_URL_API_PARTY_REGISTRY_PROXY').required().asString(),
-    // GEOTAXONOMY: env.get('REACT_APP_URL_GEOTAXONOMY').required().asString(),
-    GEOTAXONOMY:'https://api.dev.selfcare.pagopa.it/external/geo-tax/geotaxonomy'
+    GEOTAXONOMY: env.get('REACT_APP_URL_GEOTAXONOMY').required().asString(),
   },
 
   MAX_INSTITUTIONS_FETCH: env.get('REACT_APP_MAX_INSTITUTIONS_FETCH').required().asIntPositive(),
