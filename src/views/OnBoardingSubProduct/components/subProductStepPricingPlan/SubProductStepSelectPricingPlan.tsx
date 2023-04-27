bb89361876db3732f09f30e3e269fc8f723ef1ee
esignore <elena.signore@nttdata.com>
2023-04-27 18:48:10 +0200

    SELC-2239 - fixed height and step visualization

--- src/views/OnBoardingSubProduct/components/subProductStepPricingPlan/SubProductStepSelectPricingPlan.tsx
+++ src/views/OnBoardingSubProduct/components/subProductStepPricingPlan/SubProductStepSelectPricingPlan.tsx
@@ -45,97 +45,113 @@ export default function SubProductStepSelectPricingPlan({ forward }: Props) {
   };
 
   return (
-    <Box width={'100%'} height={'720px'} sx={{ backgroundColor: 'primary.main' }} mt={'-88px'}>
-      <Grid
-        container
-        direction={'column'}
-        alignContent={'centr'}
-        sx={{ alignContent: 'center' }}
-        alignItems="center"
-      >
-        {/* discount banner */}
-        <Grid item xs={12} mt={8}>
-          {discount ? (
-            <Box
-              width={'326px'}
-              height={'41px'}
-              sx={{
-                backgroundColor: 'warning.main',
-                borderRadius: theme.spacing(0.5),
-              }}
-              display="flex"
-              alignItems="center"
+    <>
+      <Box width={'100%'} sx={{ backgroundColor: 'primary.main' }} mt={'-88px'}>
+        <Grid
+          container
+          direction={'column'}
+          alignContent={'centr'}
+          sx={{ alignContent: 'center' }}
+          alignItems="center"
+        >
+          {/* discount banner */}
+          <Grid item xs={12} mt={8}>
+            {discount ? (
+              <Box
+                width={'326px'}
+                height={'41px'}
+                sx={{
+                  backgroundColor: 'warning.main',
+                  borderRadius: theme.spacing(0.5),
+                }}
+                display="flex"
+                alignItems="center"
+                justifyContent="center"
+              >
+                <Typography variant="subtitle2">
+                  {t('onBoardingSubProduct.subProductStepSelectPricingPlan.discountLabelData')}{' '}
+                </Typography>
+              </Box>
+            ) : (
+              <Box height={'41px'}></Box>
+            )}
+          </Grid>
+          {/* title section */}
+          <Grid item xs={4} mt={3} display="flex" justifyContent="center" maxWidth={'100%'}>
+            <Typography
+              variant="h2"
+              display={'flex'}
               justifyContent="center"
+              alignContent={'center'}
+              color="white"
+              textAlign="center"
             >
-              <Typography variant="subtitle2">
-                {t('onBoardingSubProduct.subProductStepSelectPricingPlan.discountLabelData')}{' '}
-              </Typography>
-            </Box>
-          ) : (
-            <Box height={'41px'}></Box>
-          )}
-        </Grid>
-
-        {/* title section */}
-        <Grid item xs={4} mt={3} display="flex" justifyContent="center" maxWidth={'100%'}>
-          <Typography
-            variant="h2"
-            display={'flex'}
-            justifyContent="center"
-            alignContent={'center'}
-            color="white"
-            textAlign="center"
-          >
-            <Trans i18nKey="onBoardingSubProduct.subProductStepSelectPricingPlan.title">
-              Passa a IO Premium e migliora le <br /> performance dei messaggi
-            </Trans>
-          </Typography>
-        </Grid>
-        {/* check section */}
-        <Grid item xs={4} mt={3} maxWidth={'100%'} direction="column">
-          <Box display={'flex'} justifyContent="center">
-            <CheckCircleIcon style={{ color: 'white' }} fontSize="small" />
-            <Typography pl={1} fontSize="fontSize" color={'white'}>
-              {t('onBoardingSubProduct.subProductStepSelectPricingPlan.firstCheckLabel')}
-            </Typography>
-          </Box>
-          <Box display={'flex'} justifyContent="center" pt={1}>
-            <CheckCircleIcon style={{ color: 'white' }} fontSize="small" />
-            <Typography pl={1} fontSize="fontSize" color={'white'}>
-              {t('onBoardingSubProduct.subProductStepSelectPricingPlan.secondCheckLabel')}
-            </Typography>
-          </Box>
-          <Box display={'flex'} justifyContent="center" pt={1}>
-            <CheckCircleIcon style={{ color: 'white' }} fontSize="small" />
-            <Typography pl={1} fontSize="fontSize" color={'white'}>
-              {t('onBoardingSubProduct.subProductStepSelectPricingPlan.thirdCheckLabel')}
-            </Typography>
-          </Box>
-        </Grid>
-        {/* info section */}
-        <Grid container item justifyContent={'center'} mt={4} mb={5}>
-          <Grid item xs={7}>
-            <Typography fontSize={'fontSize'} color="white" textAlign={'center'}>
-              <Trans i18nKey="onBoardingSubProduct.subProductStepSelectPricingPlan.infoSectionLabel">
-                Se il tuo ente ha già aderito ad app IO, scegli qual è il piano che più soddisfa le
-                sue esigenze. <br /> Il piano a carnet è attivabile una sola volta. Una volta
-                terminato il numero di messaggi del piano a <br /> carnet, si attiverà
-                automaticamente il piano a consumo.
+              <Trans i18nKey="onBoardingSubProduct.subProductStepSelectPricingPlan.title">
+                Passa a IO Premium e migliora le <br /> performance dei messaggi
               </Trans>
             </Typography>
           </Grid>
+          {/* check section */}
+          <Grid item xs={4} mt={3} maxWidth={'100%'} direction="column">
+            <Box display={'flex'} justifyContent="center">
+              <CheckCircleIcon style={{ color: 'white' }} fontSize="small" />
+              <Typography pl={1} fontSize="fontSize" color={'white'}>
+                {t('onBoardingSubProduct.subProductStepSelectPricingPlan.firstCheckLabel')}
+              </Typography>
+            </Box>
+            <Box display={'flex'} justifyContent="center" pt={1}>
+              <CheckCircleIcon style={{ color: 'white' }} fontSize="small" />
+              <Typography pl={1} fontSize="fontSize" color={'white'}>
+                {t('onBoardingSubProduct.subProductStepSelectPricingPlan.secondCheckLabel')}
+              </Typography>
+            </Box>
+            <Box display={'flex'} justifyContent="center" pt={1}>
+              <CheckCircleIcon style={{ color: 'white' }} fontSize="small" />
+              <Typography pl={1} fontSize="fontSize" color={'white'}>
+                {t('onBoardingSubProduct.subProductStepSelectPricingPlan.thirdCheckLabel')}
+              </Typography>
+            </Box>
+          </Grid>
+          {/* info section */}
+          <Grid container item justifyContent={'center'} mt={4} mb={5}>
+            <Grid item xs={7}>
+              <Typography fontSize={'fontSize'} color="white" textAlign={'center'}>
+                <Trans i18nKey="onBoardingSubProduct.subProductStepSelectPricingPlan.infoSectionLabel">
+                  Se il tuo ente ha già aderito ad app IO, scegli qual è il piano che più soddisfa
+                  le sue esigenze. <br /> Il piano a carnet è attivabile una sola volta. Una volta
+                  terminato il numero di messaggi del piano a <br /> carnet, si attiverà
+                  automaticamente il piano a consumo.
+                </Trans>
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid container item justifyContent={'center'}>
+            <Box display={'flex'}>
+              <Box>
+                <CarnetPlanCard discount={discount} forward={forward} showHeader={true} />
+              </Box>
+              <Box>
+                <ConsumptionPlanCard discount={discount} forward={forward} showHeader={true} />
+              </Box>
+            </Box>
+          </Grid>
         </Grid>
+      </Box>
+      <Box>
+        {/*  */}
         <Grid container item justifyContent={'center'}>
           <Box display={'flex'}>
             <Box>
-              <CarnetPlanCard discount={discount} forward={forward} />
+              <CarnetPlanCard discount={discount} forward={forward} showHeader={false} />
             </Box>
             <Box>
-              <ConsumptionPlanCard discount={discount} forward={forward} />
+              <ConsumptionPlanCard discount={discount} forward={forward} showHeader={false} />
             </Box>
           </Box>
         </Grid>
-        <Grid item>
+
+        {/* forward action */}
+        <Grid item display={'flex'} justifyContent="center">
           <Button
             onClick={() => onReject()}
             sx={{ mt: 5, ':hover': { backgroundColor: 'transparent' } }}
@@ -144,23 +160,25 @@ export default function SubProductStepSelectPricingPlan({ forward }: Props) {
             {t('onBoardingSubProduct.subProductStepSelectPricingPlan.btnRejectLabel')}
           </Button>
         </Grid>
-      </Grid>
-
-      <SessionModal
-        open={openExitModal}
-        title={t('onBoardingSubProduct.subProductStepSelectPricingPlan.pricingPlanExitModal.title')}
-        message={t(
-          'onBoardingSubProduct.subProductStepSelectPricingPlan.pricingPlanExitModal.subtitle'
-        )}
-        onConfirmLabel={t(
-          'onBoardingSubProduct.subProductStepSelectPricingPlan.pricingPlanExitModal.closeBtnLabel'
-        )}
-        onCloseLabel={t(
-          'onBoardingSubProduct.subProductStepSelectPricingPlan.pricingPlanExitModal.confirmBtnLabel'
-        )}
-        onConfirm={onExitPremiumFlow}
-        handleClose={handleClose}
-      />
-    </Box>
+        <SessionModal
+          open={openExitModal}
+          title={t(
+            'onBoardingSubProduct.subProductStepSelectPricingPlan.pricingPlanExitModal.title'
+          )}
+          message={t(
+            'onBoardingSubProduct.subProductStepSelectPricingPlan.pricingPlanExitModal.subtitle'
+          )}
+          onConfirmLabel={t(
+            'onBoardingSubProduct.subProductStepSelectPricingPlan.pricingPlanExitModal.closeBtnLabel'
+          )}
+          onCloseLabel={t(
+            'onBoardingSubProduct.subProductStepSelectPricingPlan.pricingPlanExitModal.confirmBtnLabel'
+          )}
+          onConfirm={onExitPremiumFlow}
+          handleClose={handleClose}
+        />
+        {/*  */}
+      </Box>
+    </>
   );
 }
