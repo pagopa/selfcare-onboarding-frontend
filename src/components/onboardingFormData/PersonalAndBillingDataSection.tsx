91656eb9ce815f4ee8ade7d5a1fd138253e0f9c9
loremedda <lorenzo.medda@nttdata.com>
2023-11-09 11:39:06 +0100

    [SELC-3362] feat: temp commit

--- src/components/onboardingFormData/PersonalAndBillingDataSection.tsx
+++ src/components/onboardingFormData/PersonalAndBillingDataSection.tsx
@@ -1,22 +1,22 @@
-import { Grid, MenuItem, Paper, TextField, Typography } from '@mui/material';
-import Autocomplete from '@mui/material/Autocomplete';
-import Checkbox from '@mui/material/Checkbox';
 import { Box, styled } from '@mui/system';
-import { theme } from '@pagopa/mui-italia';
-import { AxiosResponse } from 'axios';
 import { useContext, useEffect, useState } from 'react';
+import { Grid, TextField, Typography, Paper, MenuItem } from '@mui/material';
 import { useTranslation } from 'react-i18next';
+import Checkbox from '@mui/material/Checkbox';
+import { theme } from '@pagopa/mui-italia';
+import Autocomplete from '@mui/material/Autocomplete';
+import { AxiosResponse } from 'axios';
 import { InstitutionType, Party, StepperStepComponentProps } from '../../../types';
-import { fetchWithLogs } from '../../lib/api-utils';
-import { UserContext } from '../../lib/context';
-import { getFetchOutcome } from '../../lib/error-utils';
-import { AooData } from '../../model/AooData';
-import { GeographicTaxonomyResource } from '../../model/GeographicTaxonomies';
-import { InstitutionLocationData } from '../../model/InstitutionLocationData';
 import { OnboardingFormData } from '../../model/OnboardingFormData';
-import { UoData } from '../../model/UoModel';
-import { formatCity } from '../../utils/formatting-utils';
 import { StepBillingDataHistoryState } from '../steps/StepOnboardingFormData';
+import { AooData } from '../../model/AooData';
+import { UoData } from '../../model/UoModel';
+import { fetchWithLogs } from '../../lib/api-utils';
+import { getFetchOutcome } from '../../lib/error-utils';
+import { GeographicTaxonomyResource } from '../../model/GeographicTaxonomies';
+import { UserContext } from '../../lib/context';
+import { InstitutionLocationData } from '../../model/InstitutionLocationData';
+import { formatCity } from '../../utils/formatting-utils';
 import NumberDecimalFormat from './NumberDecimalFormat';
 
 const CustomTextField = styled(TextField)({
@@ -111,8 +111,8 @@ export default function PersonalAndBillingDataSection({
   const isAooUo = aooSelected || uoSelected;
 
   useEffect(() => {
-    if (isFromIPA && selectedParty?.istat_code) {
-      void getLocationFromIstatCode(selectedParty.istat_code);
+    if ((isFromIPA || isPA) && selectedParty?.istatCode) {
+      void getLocationFromIstatCode(selectedParty.istatCode);
     }
   }, [institutionType, selectedParty]);
 
@@ -344,8 +344,6 @@ export default function PersonalAndBillingDataSection({
                 getOptionLabel={(o) => o.city}
                 options={countries ?? []}
                 noOptionsText={t('onboardingFormData.billingDataSection.noResult')}
-                popupIcon={isFromIPA ? '' : undefined}
-                readOnly={isFromIPA}
                 ListboxProps={{
                   style: {
                     overflow: 'visible',
@@ -396,9 +394,6 @@ export default function PersonalAndBillingDataSection({
                           textTransform: 'capitalize',
                           color: theme.palette.text.secondary,
                         },
-                      '.css-177og1b-MuiInputBase-input-MuiOutlinedInput-input.Mui-disabled': {
-                        '-webkit-text-fill-color': '#0d0e0e',
-                      },
                     }}
                     disabled={isPA || isFromIPA}
                   />
