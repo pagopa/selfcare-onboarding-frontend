8d26b7245c6b1f7a1c3942198ee9c39ac24708b5
loremedda <98110186+loremedda@users.noreply.github.com>
2023-11-16 18:07:27 +0100

    [SELC-3377] fix: added bold to reaCapital and supportEmail

--- src/components/onboardingFormData/NumberDecimalFormat.tsx
+++ src/components/onboardingFormData/NumberDecimalFormat.tsx
@@ -4,7 +4,7 @@ import NumberFormat from 'react-number-format';
 const NumberDecimalFormat = React.forwardRef(function NumberDecimalFormat(props: any, ref) {
   const { onChange, ...other } = props;
   return (
-    <div style={{ fontWeight: 'normal', width: '90%' }}>
+    <div style={{ width: '90%' }}>
       <NumberFormat
         {...other}
         getInputRef={ref}
