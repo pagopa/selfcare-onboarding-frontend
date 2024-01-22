b05e88f28c93596056e86d599b629f98c5a3f7df
OraldoDoci <117348307+OraldoDoci@users.noreply.github.com>
2024-01-22 15:02:15 +0100

    SELC-3747 change copy of errror message

--- src/locale/it.ts
+++ src/locale/it.ts
@@ -454,7 +454,7 @@ export default {
       invalidMailSupport: 'L’indirizzo email non è valido',
       invalidShareCapitalField: 'Il campo capitale sociale non è valido',
       vatNumberAlreadyRegistered: 'La P. IVA che hai inserito è già stata registrata.',
-      vatNumberVerificationError: 'Si è verificato un errore durante la verifica della P. IVA.',
+      vatNumberVerificationError: 'Non è stato possibile verificare la P.IVA',
       centralPartyLabel: 'Ente centrale',
       businessName: 'Ragione sociale',
       aooName: 'Denominazione AOO',
