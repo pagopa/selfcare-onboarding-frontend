28007ef7d0db2d8b9f1b296d0c5ded64a514e05a
Gabriele Maiocchi <107944380+GabrieleMaiocchiFilippo@users.noreply.github.com>
2024-09-16 17:01:54 +0200

    [Prv-Feature-Flag] feat: added private Feature Flag

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -228,7 +228,7 @@ export const institutionType4Product = (productId: string | undefined) => {
           it.labelKey === 'sa' ||
           (ENV.SCP_INFOCAMERE.SHOW && it.labelKey === 'scp') ||
           it.labelKey === 'as' ||
-          (ENV.ENV !== 'PROD' && it.labelKey === 'prv')
+          (ENV.PRV.SHOW && it.labelKey === 'prv')
       );
     case 'prod-pn':
       return institutionTypes.filter((it) => it.labelKey === 'pa');
@@ -249,7 +249,7 @@ export const institutionType4Product = (productId: string | undefined) => {
           it.labelKey === 'gsp' ||
           (ENV.ENV !== 'PROD' && it.labelKey === 'psp') ||
           (ENV.PT.SHOW_PT ? it.labelKey === 'pt' : '') ||
-          it.labelKey === 'oth'
+          (ENV.PURE_PRV.SHOW ? it.labelKey === 'oth' : '')
       );
     case 'prod-io-sign':
       return institutionTypes.filter((it) => it.labelKey === 'pa' || it.labelKey === 'gsp');
