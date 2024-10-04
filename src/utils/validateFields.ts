c2ac207c098bc1cda575bf545d5a48882045539b
Gabriele Maiocchi <107944380+GabrieleMaiocchiFilippo@users.noreply.github.com>
2024-10-04 17:16:27 +0200

    [SELC-5705] fix: businessRegisterPlace field has to be optional

--- src/utils/validateFields.ts
+++ src/utils/validateFields.ts
@@ -94,10 +94,7 @@ export const validateFields = (
         ? t('onboardingFormData.billingDataSection.pspDataSection.invalidCommercialRegisterNumber')
         : undefined,
     businessRegisterPlace:
-      (institutionType === 'SA' || (institutionType === 'PRV' && productId === 'prod-pagopa')) &&
-      !values.businessRegisterPlace
-        ? requiredError
-        : undefined,
+      institutionType === 'SA' && !values.businessRegisterPlace ? requiredError : undefined,
     registrationInRegister:
       isPaymentServiceProvider && !values.registrationInRegister ? requiredError : undefined,
     dpoAddress: isPaymentServiceProvider && !values.dpoAddress ? requiredError : undefined,
