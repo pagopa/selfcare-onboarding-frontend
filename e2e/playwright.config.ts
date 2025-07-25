927d9619e8f32fe527c6dc80e52463c9efc1e20f
Giampiero Ferrara <giampiero.ferrara@emeal.nttdata.com>
2025-07-25 12:24:43 +0200

    Fixed config

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -20,9 +20,15 @@ export default defineConfig({
   fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  workers:1,
+
+   /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  reporter: 'html',
   use: {
     locale: 'it-IT',
     /* Base URL to use in actions like `await page.goto('/')`. */
