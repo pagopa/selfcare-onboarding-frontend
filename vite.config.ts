b4997b88c06f61d5a8802eccc24e660fcc79f01d
GabrieleMaiocchiFilippo <fmaiocch@emeal.nttdata.com>
2026-02-05 13:27:09 +0100

    update dependencies and tsconfig.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import react from '@vitejs/plugin-react';
 import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 import { createHtmlPlugin } from 'vite-plugin-html';
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
@@ -8,10 +9,12 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [
       react(),
+      svgr({
+        exportAsDefault: false,
+      }),
       createHtmlPlugin({
         inject: {
           data: {
-            // Inietta variabili nell'HTML
             VITE_URL_CDN: env.VITE_URL_CDN,
             VITE_ONE_TRUST_BASE_URL: env.VITE_ONE_TRUST_BASE_URL,
             VITE_ONETRUST_DOMAIN_ID: env.VITE_ONETRUST_DOMAIN_ID,
@@ -29,7 +32,6 @@ export default defineConfig(({ mode }) => {
       sourcemap: true,
     },
     define: {
-      // Espone tutte le VITE_* come process.env per compatibilità con env-var
       'process.env': Object.fromEntries(
         Object.entries(env).filter(([key]) => key.startsWith('VITE_'))
       ),
