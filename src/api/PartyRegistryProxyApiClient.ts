fe22983363946fe8d809cb61008a633e117b8724
GabrieleMaiocchiFilippo <fmaiocch@emeal.nttdata.com>
2026-05-29 17:30:17 +0200

    Feat: Added setup for party-registry-proxy

---
+++ src/api/PartyRegistryProxyApiClient.ts
@@ -0,0 +1,48 @@
+import i18n from '@pagopa/selfcare-common-frontend/lib/locale/locale-utils';
+import { appStateActions } from '@pagopa/selfcare-common-frontend/lib/redux/slices/appStateSlice';
+import {
+  buildFetchApi,
+  extractResponse,
+} from '@pagopa/selfcare-common-frontend/lib/utils/api-utils';
+import { storageTokenOps } from '@pagopa/selfcare-common-frontend/lib/utils/storage';
+import { store } from '../redux/store';
+import { ENV } from '../utils/env';
+import { createClient, WithDefaultsT } from './generated/party-registry-proxy/client';
+import { GeographicTaxonomyResource } from './generated/party-registry-proxy/GeographicTaxonomyResource';
+
+const withBearerAuth: WithDefaultsT<'bearerAuth'> = (wrappedOperation) => (params: any) => {
+  const token = storageTokenOps.read();
+  return wrappedOperation({
+    ...params,
+    bearerAuth: `Bearer ${token}`,
+  });
+};
+
+const apiClient = createClient({
+  baseUrl: ENV.URL_API.PARTY_REGISTRY_PROXY,
+  basePath: '',
+  fetchApi: buildFetchApi(),
+  withDefaults: withBearerAuth,
+});
+
+const onRedirectToLogin = () =>
+  store.dispatch(
+    appStateActions.addError({
+      id: 'tokenNotValid',
+      error: new Error(),
+      techDescription: 'token expired or not valid',
+      toNotify: false,
+      blocking: false,
+      displayableTitle: i18n.t('session.expired.title'),
+      displayableDescription: i18n.t('session.expired.message'),
+    })
+  );
+
+export const PartyRegistryProxyApi = {
+  getTaxonomiesByQuery: async (query: string): Promise<Array<GeographicTaxonomyResource>> => {
+    const result = await apiClient.retrieveGeoTaxonomiesByDescriptionUsingGET({
+      description: query,
+    });
+    return extractResponse(result, 200, onRedirectToLogin);
+  },
+};
